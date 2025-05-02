57c3f992f8463e4deb7143e7e05a06b15449bcc0
TarekBesrour <tarek.besrour@gmail.com>
2025-05-02 10:55:47 +0100

    connection à DB

--- tms-backend/app.js
+++ tms-backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 app.use(cors());
 
@@ -8,3 +13,8 @@ app.get('/', (req, res) => res.send('Hello from backend!'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/test-db', async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  });
